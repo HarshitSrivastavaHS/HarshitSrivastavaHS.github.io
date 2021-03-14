95dc8201fdc3831f9da239329cd5acd5c3f4d4ef
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-14 20:04:38 +0530

    Create script.js

---
+++ GARUDA/script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  document.getElementById("loadContain").style.display = "none";
+  document.querySelector("header").style.height =
+    document.querySelector("header").offsetHeight + "px";
+};
